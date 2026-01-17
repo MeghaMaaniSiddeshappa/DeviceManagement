122c6e58fe2a2303597947198093b707c6100894
Megha <maani.megha@gmail.com>
2026-01-17 16:57:07 +0100

    feat: initial project setup with draft APIs for device management

---
+++ devices-api/src/test/java/com/dm/devicesapi/DevicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm.devicesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
